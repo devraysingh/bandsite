2282ef5a72163b71cd3b1a90f7c1cab73e996c2c
devraysingh <raysahni98@gmail.com>
2023-02-19 20:22:41 -0500

    comments shows js

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,88 @@
+    const showTimes = [
+        {
+            date: "Mon Sept 06 2021",
+            venue: "Ronald Lane",
+            location: "San Francisco, CA"
+        },
+        {
+            date: "Tue Sept 21 2021",
+            venue: "Pier 3 East",
+            location: "San Francisco, CA"
+        },
+        {
+            date: "Fri Oct 15 2021",
+            venue: "View Lounge",
+            location: "San Francisco, CA"
+        },
+        {
+            date: "Sat Nov 06 2021",
+            venue: "Hyatt Agency",
+            location: "San Francisco, CA"
+        },
+        {
+            date: "Fri Nov 26 2021",
+            venue: "Moscow Center",
+            location: "San Francisco, CA"
+        },
+        {
+            date: "Wed Dec 15 2021",
+            venue: "Press Club",
+            location: "San Francisco, CA"
+        }
+    ];
+    
+
+
+    const showsSchedule = document.querySelector(".shows__schedule");
+    console.log(showsSchedule);
+
+    showTimes.forEach((show) => {
+        //Element Creation for Show Card 
+        const showCard  = document.createElement("div");
+        const dateLabel = document.createElement("p");
+        const showDate = document.createElement("p");
+        const venueLabel = document.createElement("p");
+        const venueName = document.createElement("p");
+        const locLabel = document.createElement("p");
+        const locName = document.createElement("p");
+        const button = document.createElement("button");
+        const newSec =  document.createElement("hr");
+  
+
+        //data insertion
+        dateLabel.innerText = "DATE";
+        venueLabel.innerText = "VENUE";
+        locLabel.innerText = "LOCATION";
+        showDate.innerText = show.date;
+        venueName.innerText = show.venue;
+        locName.innerText = show.location;
+        button.innerText = "BUY TICKETS";
+
+
+        //adding classes
+        showCard.classList.add("shows__card");
+        dateLabel.classList.add("shows__label");
+        showDate.classList.add("shows__date");
+        venueLabel.classList.add("shows__label");
+        venueName.classList.add("shows__venue");
+        locLabel.classList.add("shows__label");
+        locName.classList.add("shows__loc");
+        button.classList.add("shows__btn");
+        newSec.classList.add("shows__new-sec");
+
+
+
+        //html insertion
+        showCard.appendChild(dateLabel);
+        showCard.appendChild(showDate);
+        showCard.appendChild(venueLabel);
+        showCard.appendChild(venueName);
+        showCard.appendChild(locLabel);
+        showCard.appendChild(locName);
+        showCard.appendChild(button);
+
+
+        showsSchedule.appendChild(showCard);
+        showsSchedule.appendChild(newSec);
+
+    });
---
+++ scripts/index-page.js
@@ -0,0 +1,102 @@
+
+    const comments = [
+        {
+            name: "Conner Walton",
+            date: "02/17/2021",
+            comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+        },
+        {
+            name: "Emilie Beach",
+            date: "01/09/2021",
+            comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+        },
+        {
+            name: "Miles Acosta",
+            date: "12/20/2020",
+            comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+        }
+    ];
+
+
+    const form = document.querySelector(".conv__form");
+
+
+    const commentsSection = document.querySelector(".comments");
+
+    console.log(form);
+
+    console.log(commentsSection);
+
+    function displayComment() {
+
+        commentsSection.innerHTML = "";
+
+        comments.forEach((comment) => {
+
+            const commentContainer = document.createElement("div");
+            const avatarContainer = document.createElement("div");
+            const userDetail = document.createElement("div");
+            const nameContainer = document.createElement("div");
+            const dateContainer = document.createElement("div");
+            const name = document.createElement("p");
+            const date = document.createElement("p");
+            const commentDes = document.createElement("div");
+            const userComment = document.createElement("div");
+            const newSec = document.createElement('hr');
+
+            name.innerText = comment.name;
+            date.innerText = comment.date;
+            commentDes.innerText = comment.comment;
+
+
+
+            nameContainer.appendChild(name);
+            dateContainer.appendChild(date);
+            userDetail.appendChild(nameContainer);
+            userDetail.appendChild(dateContainer);
+
+
+            avatarContainer.classList.add("comments__avatar-cont");
+            userDetail.classList.add("comments__user-detail");
+            name.classList.add("comments__name");
+            date.classList.add("comments__date");
+            commentDes.classList.add("comments__comment-des");
+            userComment.classList.add("comments__user-cont");
+            newSec.classList.add("comments__new-sec");
+            commentContainer.classList.add("comments__comment-cont");
+
+
+            userComment.appendChild(userDetail);
+            userComment.appendChild(commentDes);
+
+            commentContainer.appendChild(avatarContainer);
+            commentContainer.appendChild(userComment);
+        
+
+            commentsSection.appendChild(commentContainer);
+            commentsSection.appendChild(newSec);
+        });
+        
+    };
+
+    displayComment();
+
+    form.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+        let date = new Date().toLocaleDateString('en-us');
+        console.log(e.target.name.value);
+
+        let newComment = {
+            name: e.target.name.value,
+            date: date,
+            comment: e.target.comment.value
+        };
+        console.log(newComment);
+        e.target.name.value = "";
+        e.target.comment.value = "";
+        comments.unshift(newComment);
+        displayComment();
+ 
+ 
+    });
