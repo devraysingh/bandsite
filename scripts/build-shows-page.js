1d2a54c6693c7839d590911e6108d4b3fb811bb1
devraysingh <raysahni98@gmail.com>
2023-02-26 15:31:23 -0500

    Feedback 2, Shows Axios Get, Shows Bugs

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,121 +1,79 @@
-    const showTimes = [
-        {
-            date: "Mon Sept 06 2021",
-            venue: "Ronald Lane",
-            location: "San Francisco, CA"
-        },
-        {
-            date: "Tue Sept 21 2021",
-            venue: "Pier 3 East",
-            location: "San Francisco, CA"
-        },
-        {
-            date: "Fri Oct 15 2021",
-            venue: "View Lounge",
-            location: "San Francisco, CA"
-        },
-        {
-            date: "Sat Nov 06 2021",
-            venue: "Hyatt Agency",
-            location: "San Francisco, CA"
-        },
-        {
-            date: "Fri Nov 26 2021",
-            venue: "Moscow Center",
-            location: "San Francisco, CA"
-        },
-        {
-            date: "Wed Dec 15 2021",
-            venue: "Press Club",
-            location: "San Francisco, CA"
-        }
-    ];
-    
-
-
     const showsSchedule = document.querySelector(".shows__schedule");  
-    const tabDesMenu = document.createElement("div");
-    let dateLabel = document.createElement("p");
-    let venueLabel = document.createElement("p");
-    let locLabel = document.createElement("p");
-    const spacerContainer = document.createElement("div");
-    dateLabel.classList.add("shows__date-tab");
-    venueLabel.classList.add("shows__venue-tab");
-    locLabel.classList.add("shows__loc-tab");
-    spacerContainer.classList.add("shows__loc-spacer");
-    tabDesMenu.classList.add("shows__title-show");
-    dateLabel.innerText = "DATE";
-    venueLabel.innerText = "VENUE";
-    locLabel.innerText = "LOCATION";
-    tabDesMenu.appendChild(dateLabel);
-    tabDesMenu.appendChild(venueLabel);
-    tabDesMenu.appendChild(locLabel);
-    tabDesMenu.appendChild(spacerContainer);
 
-    showsSchedule.appendChild(tabDesMenu);
+    axios.get("https://project-1-api.herokuapp.com/showdates?api_key=f8ca13e4-fee9-4e74-8598-bc3b89d638b4")
+    .then((res) => {
+        let showsTimes = res.data;
+        showsCard(showsTimes);
+        const cards = document.querySelectorAll('.shows__card');
+        cards.forEach(i => i.addEventListener('click', cardClick));
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
+    showsMenu();
 
-
-
-
-    showTimes.forEach((show) => {
-        //Element Creation for Show Card 
-        dateLabel = document.createElement("p");
-        venueLabel = document.createElement("p");
-        locLabel = document.createElement("p");
-        const showCard  = document.createElement("div");
-        const showDate = document.createElement("p");   
-        const venueName = document.createElement("p"); 
-        const locName = document.createElement("p");
-        const button = document.createElement("button");
-        const newSec =  document.createElement("hr");
-
-        //data insertion
+    function showsMenu(){
+        const showsSchedule = document.querySelector(".shows__schedule");  
+        const tabDesMenu = document.createElement("div");
+        const dateLabel = document.createElement("p");
+        const venueLabel = document.createElement("p");
+        const locLabel = document.createElement("p");
+        const spacerContainer = document.createElement("div");
+        dateLabel.classList.add("shows__date-tab");
+        venueLabel.classList.add("shows__venue-tab");
+        locLabel.classList.add("shows__loc-tab");
+        spacerContainer.classList.add("shows__loc-spacer");
+        tabDesMenu.classList.add("shows__title-show");
         dateLabel.innerText = "DATE";
         venueLabel.innerText = "VENUE";
         locLabel.innerText = "LOCATION";
-        showDate.innerText = show.date;
-        venueName.innerText = show.venue;
-        locName.innerText = show.location;
-        button.innerText = "BUY TICKETS";
+        tabDesMenu.append(dateLabel, venueLabel, locLabel, spacerContainer);
+        showsSchedule.append(tabDesMenu);
+    };
 
+    function showsCard(showsTimes) {
+        for (const show of showsTimes) {
+            //Element Creation for Show Card 
+            const showsSchedule = document.querySelector(".shows__schedule");  
+            const dateLabel = document.createElement("p");
+            const venueLabel = document.createElement("p");
+            const locLabel = document.createElement("p");
+            const showCard  = document.createElement("div");
+            const showDate = document.createElement("p");   
+            const venueName = document.createElement("p"); 
+            const locName = document.createElement("p");
+            const button = document.createElement("button");
+            const date = new Date(show.date);
+            const conditions = { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' };
+            const dateString = date.toLocaleDateString('en-US', conditions).replace(/,/g, '');
+            //data insertion
+            dateLabel.innerText = "DATE";
+            venueLabel.innerText = "VENUE";
+            locLabel.innerText = "LOCATION";
+            button.innerText = "BUY TICKETS";
+            showDate.innerText = dateString;
+            venueName.innerText = show.place;
+            locName.innerText = show.location;
+            //adding classes
+            showCard.classList.add("shows__card");
+            dateLabel.classList.add("shows__label");
+            showDate.classList.add("shows__date");
+            venueLabel.classList.add("shows__label");
+            venueName.classList.add("shows__venue");
+            locLabel.classList.add("shows__label");
+            locName.classList.add("shows__loc");
+            button.classList.add("shows__btn");
+            //html insertion
+            showCard.append(dateLabel, showDate, venueLabel, venueName, locLabel, locName, button);
+            showsSchedule.appendChild(showCard);
+        };
+    };
 
-        //adding classes
-        showCard.classList.add("shows__card");
-        dateLabel.classList.add("shows__label");
-        showDate.classList.add("shows__date");
-        venueLabel.classList.add("shows__label");
-        venueName.classList.add("shows__venue");
-        locLabel.classList.add("shows__label");
-        locName.classList.add("shows__loc");
-        button.classList.add("shows__btn");
-        newSec.classList.add("shows__new-sec");
-
-
-
-        //html insertion
-        showCard.appendChild(dateLabel);
-        showCard.appendChild(showDate);
-        showCard.appendChild(venueLabel);
-        showCard.appendChild(venueName);
-        showCard.appendChild(locLabel);
-        showCard.appendChild(locName);
-        showCard.appendChild(button);
-
-
-        showsSchedule.appendChild(showCard);
-        showsSchedule.appendChild(newSec);
-
-    });
-
-
-
-
-    showsSchedule.addEventListener("click", (e) => {
-        const showCards = document.querySelectorAll('.shows__card');
-
-        showCards.forEach(element => element.classList.remove('shows__card--clicked'))
+    function cardClick(e){
         e.preventDefault();
+        const cards = document.querySelectorAll('.shows__card');
+        cards.forEach(element => element.classList.remove('shows__card--clicked'))
         if ((e.target.nodeName === 'DIV') && e.target.classList.contains('shows__card')) {
             e.target.classList.add("shows__card--clicked");
         } else if(e.target.nodeName === 'HR') {
@@ -126,5 +84,7 @@
             if(clickedDiv.classList.contains("shows__card"))
                 clickedDiv.classList.add("shows__card--clicked");
         }
-    });
+    };
 
+
+    
